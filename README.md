920235bc2870444df10a657f5720d603b146bc33
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2021-12-28 02:33:15 +0000

    feat: add JWT unlock

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # Lit-CloudFlare Unlock SDK
 
-Run `yarn build` to minify .js and .css 
+## Build
 
-output at `./dist`
+Add/Change version at `.env`
+
+```
+version=0.0.1
+```
+
+Then, run the build command to minify `lit-unlock.js` and `lit-unlock.css`
+
+```
+yarn build
+``` 
+
+The output will be stored at `./dist` based on its version number
+
+```
+./dist/0.0.1/lit-unlock.min.js
+./dist/0.0.1/lit-unlock.min.css
+```
+
+## Deploy
+run
+
+```
+yarn deploy
+
+// which is equivalent to
+yarn build && vercel ./dist --prod
+```
+
+> This SDK is deployed to Vercel.app
--- lit-unlock.js
+++ lit-unlock.js
@@ -1,4 +1,3 @@
-
 // ----- helper
 //
 // convert data URI to blob
@@ -17,11 +16,22 @@ function dataURItoBlob(dataURI) {
     return blob;
 }
 
+// 
+// Turn Uint8Array to Hex
+// @param { String } buffer
+// @returns { String } 0x...
+//
 buf2hex = (buffer) => [...new Uint8Array(buffer)]
         .map((x) => x.toString(16).padStart(2, "0"))
         .join("");
 
 // ----- unlock
+
+//
+// (Deprecated) Decrypt videoId
+// @param { HTMLElement } element
+// @returns { void }
+// 
 async function onClickedUnlock(e){
     var data = JSON.parse(atob(e.getAttribute('data-lit')));
 
@@ -49,6 +59,11 @@ async function onClickedUnlock(e){
     }
 }
 
+// 
+// Get video id
+// @param { HTMLElmenet } e
+// @returns { void } 
+//
 async function onClickedUnlockJWT(e){
 
     const data = JSON.parse(atob(e.getAttribute('data-lit')));
@@ -56,55 +71,56 @@ async function onClickedUnlockJWT(e){
     
     const resourceId = JSON.parse(atob(data['resourceId_base64']));
     console.log(resourceId);
-    // const encryptedZip = dataURItoBlob(data['encryptedZip']);
-    // const toDecrypt = buf2hex(new Uint8Array(atob(data['encryptedSymmetricKey']).split(',').map((x) => parseInt(x))));
     const chain = accessControlConditions[0].chain;
     const authSig = await LitJsSdk.checkAndSignAuthMessage({chain: chain});
+
+    // -- prepare
     const jwt = await litNodeClient.getSignedToken({ accessControlConditions, chain, authSig, resourceId });
-    console.warn("JWT:", jwt);
+    const url = `http://127.0.0.1:8787/api/video_id?jwt=${jwt}`;
+    console.warn(url);
 
-    // const decryptedSymmetricKey = await window.litNodeClient.getEncryptionKey({
-    //     accessControlConditions,
-    //     toDecrypt,
-    //     chain,
-    //     authSig
-    // });
+    // -- execute
+    const res = await fetch(url);
+    const token = await res.json();
+    
+    e.src = `https://iframe.videodelivery.net/${token}`;
+    e.parentElement.classList.add('active');
 
-    // const decryptedFiles = await LitJsSdk.decryptZip(encryptedZip, decryptedSymmetricKey);
-    // const decryptedString = await decryptedFiles["string.txt"].async("text"); // video id
+}
 
-    // if(decryptedString != null & decryptedString != ''){
-    //     console.log("Unlocked");
-    //     const url = `https://iframe.videodelivery.net/${decryptedString}`;
-    //     e.src = url;
-    //     e.parentElement.classList.add('active');
-    // }
+// 
+// Manipulate elements and create a new wrapper
+// @param { HTMLElement } wrapper
+// @returns { HTMLElement, HTMLElement } btn, wrapper
+//
+function manipulateWrapper(wrapper){
+    var iframe = wrapper.querySelector('iframe');
+    var btn = wrapper.querySelector('button');
+    var text = iframe.getAttribute('data-readable-conditions');
+    var description = document.createElement('div');
+    description.classList.add('lit-video-description');
+    var overlay = document.createElement('div');
+    overlay.classList.add('lit-video-overlay');
 
+    var info = document.createElement('span');
+    info.classList.add('lit-video-info');
+    info.innerText = text;
+    wrapper.insertBefore(overlay, iframe);
+    wrapper.appendChild(description);
+    description.appendChild(btn);
+    description.appendChild(info);
+
+    return {btn, wrapper, iframe};
 }
 
 
 // mounted
 (() => {
-    [...document.getElementsByClassName('lit-video-wrapper')].forEach((wrapper) => {
-        var iframe = wrapper.querySelector('iframe');
-        var btn = wrapper.querySelector('button');
-        var text = iframe.getAttribute('data-readable-conditions');
-        var description = document.createElement('div');
-        description.classList.add('lit-video-description');
-        var overlay = document.createElement('div');
-        overlay.classList.add('lit-video-overlay');
+    [...document.getElementsByClassName('lit-video-wrapper')].forEach((_wrapper) => {
+        
+        const {btn, wrapper, iframe} = manipulateWrapper(_wrapper);
+        
+        [btn, wrapper].forEach((e) => e.addEventListener('click', () => onClickedUnlockJWT(iframe)));
 
-        var info = document.createElement('span');
-        info.classList.add('lit-video-info');
-        info.innerText = text;
-        wrapper.insertBefore(overlay, iframe);
-        wrapper.appendChild(description);
-        description.appendChild(btn);
-        description.appendChild(info);
-
-        // btn.parentElement.append(wrapper);
-        // wrapper.innerHTML = btn;
-        btn.addEventListener('click', () => onClickedUnlockJWT(iframe));
-        wrapper.addEventListener('click', () => onClickedUnlockJWT(iframe));
     });
 })();
