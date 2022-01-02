579c9e74c3d569f68b42d7ce487b3df7e99fa96b
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2022-01-02 01:41:38 +0000

    feat: add label to live component

--- lit-unlock.js
+++ lit-unlock.js
@@ -36,14 +36,14 @@ async function onClickedUnlock(e){
     const accessControlConditions = JSON.parse(atob(data['accessControlConditions']));
     
     const resourceId = JSON.parse(atob(data['resourceId_base64']));
-    console.log(resourceId);
+    console.log(`🔓resourceId: ${JSON.stringify(resourceId)}`);
     const chain = accessControlConditions[0].chain;
     const authSig = await LitJsSdk.checkAndSignAuthMessage({chain: chain});
 
     // -- prepare
     const lit_id = document.getElementById('lit-server');
     const server = lit_id.getAttribute('src').split('?server=')[1];
-    console.log(`👉 SERVER: ${server}`);
+    console.log(`🔓 SERVER: ${server}`);
     const jwt = await litNodeClient.getSignedToken({ accessControlConditions, chain, authSig, resourceId });
     const url = `${server}/api/video_id?jwt=${jwt}`;
     console.warn(url);
@@ -78,6 +78,19 @@ function manipulateWrapper(wrapper){
     wrapper.appendChild(description);
     description.appendChild(btn);
     description.appendChild(info);
+
+    // If this stream type is live
+    if(JSON.parse(atob((JSON.parse(atob(iframe.getAttribute('data-lit')))).resourceId_base64)).extraData == 'live'){
+        var divLive = document.createElement('div');
+        divLive.classList.add('lit-video-live');
+        divLive.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
+        </svg>
+        `
+        wrapper.appendChild(divLive);
+
+    }
 
     return {btn, wrapper, iframe};
 }
